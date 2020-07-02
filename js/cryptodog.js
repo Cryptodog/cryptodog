e73434892b8f684310d3a4ab20ffa98fa1b62852
ayyghost <ayyghost@users.noreply.github.com>
2020-07-02 17:51:37 -0400

    Move remaining window event handlers to cryptodog.js, delete ui.js

--- js/cryptodog.js
+++ js/cryptodog.js
@@ -106,7 +106,24 @@ if (typeof (window) !== 'undefined') {
 			Cryptodog.color.reset();
 		};
 
-		Cryptodog.UI.windowEventBindings();
+		// Prevent accidental window close.
+		window.addEventListener('beforeunload', (event) => {
+			if (Object.keys(Cryptodog.buddies).length) {
+				event.preventDefault();
+				event.returnValue = '';
+			}
+		});
+
+		// Determine whether we are showing a top margin
+		// Depending on window size
+		$(window).resize(function () {
+			if ($(window).height() < 650) {
+				$('#bubble').css('margin-top', '0');
+			} else {
+				$('#bubble').css('margin-top', '2%');
+			}
+		});
+		$(window).resize();
 
 		$('#version').text(Cryptodog.version);
 		$('#bubble').show();
