ab1fc25abf3c87292c904cceab04cc888e7fcc43
ayyghost <ayyghost@users.noreply.github.com>
2024-03-10 16:32:13 +0000

    Update for Chrome Manifest V3

--- background/background.js
+++ background/background.js
@@ -1,9 +1,9 @@
 // On extension install
 chrome.runtime.onInstalled.addListener(function(details) {
-    chrome.tabs.create({'url': chrome.extension.getURL('index.html')});
+    chrome.tabs.create({'url': chrome.runtime.getURL('index.html')});
 });
 
 // On extension icon click
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('index.html')});
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.tabs.create({'url': chrome.runtime.getURL('index.html')});
 });
